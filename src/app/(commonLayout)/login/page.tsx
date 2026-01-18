5bca163ef8e4d20a91f8c377af5261e8c73d1353
sayed725 <ssayed72533@gmail.com>
2026-01-18 11:58:28 +0600

    added login and register form

--- src/app/(commonLayout)/login/page.tsx
+++ src/app/(commonLayout)/login/page.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import { LoginForm } from "@/components/modules/authentication/login-form"
 
-const LoginPage = () => {
+export default function Page() {
   return (
-    <div>LoginPage</div>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
   )
 }
-
-export default LoginPage
--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,6 +1,14 @@
-import { Button } from "@/components/ui/button";
 
-export default function Home() {
+
+import { Button } from "@/components/ui/button";
+import { authClient } from "@/lib/auth-client";
+
+export default async function Home() {
+
+const session = await authClient.getSession();
+console.log(session);
+
+
   return (
     <div>
       <div>
