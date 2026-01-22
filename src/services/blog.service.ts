dd716bcd88109726a5d3f1adccd6c77306708866
sayed725 <ssayed72533@gmail.com>
2026-01-22 11:24:30 +0600

    added blog details page

--- src/services/blog.service.ts
+++ src/services/blog.service.ts
@@ -59,4 +59,24 @@ export const blogService = {
       return { data: null, error: { message: "Something Went Wrong" } };
     }
   },
+
+
+  getBlogById: async function (id: string) {
+    try {
+      const res = await fetch(`${API_URL}/posts/${id}`);
+
+      const data = await res.json();
+
+      return { data: data, error: null };
+    } catch (err) {
+      console.error(err);
+      return { data: null, error: { message: "Something Went Wrong" } };
+    }
+  },
+
+
+
 };
+
+
+
